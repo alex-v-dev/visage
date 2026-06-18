8fba4f316516cb75f3ae03c827baa616a519daa5
Alexandre Veaux <av.veaux@gmail.com>
2026-06-18 11:28:21 -0400

    More progress on build changes. Sort of works in static build mode

--- build.bat
+++ build.bat
@@ -10,12 +10,14 @@ mkdir build 2>nul
 cmake -B build ^
   -D CMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
   -D CMAKE_PREFIX_PATH=%INSTALL_DIR% ^
-  -D BUILD_SHARED_LIBS=ON ^
-  -D VISAGE_BUILD_EXAMPLES=OFF ^
+  -D BUILD_SHARED_LIBS=OFF ^
+  -D VISAGE_BUILD_EXAMPLES=ON ^
   -D VISAGE_BUILD_TESTS=OFF ^
   -D VISAGE_SYSTEM_BGFX=ON ^
   -D VISAGE_SYSTEM_FREETYPE=ON ^
-  -D bgfx_DIR=Z:\avDependencies-msvc2022-reldeb-shared\BGFX\lib\cmake\bgfx
+  -D bgfx_DIR=Z:\avDependencies-msvc2022-reldeb-shared\BGFX\lib\cmake\bgfx ^
+  -D Freetype_DIR=Z:\avDependencies-msvc2022-reldeb-shared\FreeType\lib\cmake\freetype
+
   .
 
 cmake --build build --config %BUILD_TYPE% --target install
