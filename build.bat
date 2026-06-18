0f9e3992a9eea0a8d12825ab8948804da4bc7fdb
Alexandre Veaux <av.veaux@gmail.com>
2026-06-18 18:22:20 -0400

    Build script tweak

--- build.bat
+++ build.bat
@@ -3,14 +3,14 @@ setlocal
 
 set INSTALL_DIR=Z:\avDependencies-msvc2022-reldeb-shared\Visage
 set BUILD_TYPE=%~2
-if "%BUILD_TYPE%"=="" set BUILD_TYPE=RelWithDebInfo
+if "%BUILD_TYPE%"=="" set BUILD_TYPE=Release
 
 mkdir build 2>nul
 
 cmake -B build ^
   -D CMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
   -D CMAKE_PREFIX_PATH=%INSTALL_DIR% ^
-  -D BUILD_SHARED_LIBS=ON ^
+  -D BUILD_SHARED_LIBS=OFF ^
   -D VISAGE_BUILD_EXAMPLES=ON ^
   -D VISAGE_BUILD_TESTS=OFF ^
   -D VISAGE_SYSTEM_BGFX=ON ^
